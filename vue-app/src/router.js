816af27371875217d5e7bff4063c9712e35ccca8
Jae Kim <jaekim0908@gmail.com>
2020-06-04 01:19:57 -0700

    restore router.js

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -27,8 +27,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ './views/about.vue'
-        ),
+        import(/* webpackChunkName: "about" */ './views/about.vue'),
     },
     {
       path: '*',
