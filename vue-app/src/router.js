d0312fb3b74e1ad0c92a64358154e1f800e41388
Jae Kim <jaekim0908@gmail.com>
2020-06-04 01:05:55 -0700

    remove videos route

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -21,21 +21,14 @@ export default new Router({
         ),
     },
     {
-      path: '/videos',
-      name: 'videos',
-      component: () =>
-        import(
-            /* webpackChunkName: "products" */ './views/products/products.vue'
-        ),
-    },
-    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ './views/about.vue'),
+        import(/* webpackChunkName: "about" */ './views/about.vue'
+        ),
     },
     {
       path: '*',
